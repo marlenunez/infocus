886fbc22503eaa5c19e6cdbfc5b857f9ec47329c
marlenunez <marleny@marlenynunez.com>
2013-07-31 21:03:04 -0400

    Further code cleanup
    Got rid of document.write, re-using the existing code instead.

--- infocus.js
+++ infocus.js
@@ -47,22 +47,18 @@ var MN_infocus = (function () {
   $('div.dek script, .if1280 .imgCap a, .if1280 .imgCap span').remove();
 
 // Create the new html code for the gallery
-  html_code += '<html>\n';
-  html_code += '<head>\n';
-  html_code += '<title>' + document.title + '</title>\n';
   html_code += '<style type="text/css">\n';
   html_code += 'body { background: #282828; color: #FFEEAA; font-family: helvetica,arial; font-size: 36px; font-weight: bold;}\n';
-  html_code += 'h2 { font-size: 18px; margin: 0; padding: 30px; position: absolute; right: 0; bottom: 0; text-shadow: 2px 2px 3px #000000; opacity: 0.1;}\n';
+  html_code += 'div.gallery_date { font-size: 18px; margin: 0; padding: 30px; position: absolute; right: 0; bottom: 0; text-shadow: 2px 2px 3px #000; opacity: 0.1; z-index: 900;}\n';
   html_code += '#container { width: 1200px; margin: 0 auto;}\n';
   html_code += 'h1, div.date, p.intro { font-size: 32px; line-height: 42px; padding: 0 30px;}\n';
   html_code += 'p.intro { color: #f6f6f6;}\n';
-  html_code += 'p.caption { top: 0; display: block; margin: 0; padding: 30px; position: absolute; text-shadow: 2px 2px 3px #000000;}\n';
-  html_code += 'p.caption:hover { opacity: 1; transition-property: opacity; transition-duration: 1s;}\n';
-  html_code += 'h2:hover { opacity: 1; transition-property: opacity; transition-duration: 1s;}\n';
+  html_code += 'p.gallery_caption { font-size: 26px; line-height: 30px; top: 0; display: block; margin: 0; padding: 30px; position: absolute; text-shadow: 2px 2px 3px #000;}\n';
+  html_code += 'p.gallery_caption:hover { opacity: 0; transition-property: opacity; transition-duration: 1s;}\n';
+  html_code += 'div.gallery_date:hover { opacity: 1; transition-property: opacity; transition-duration: 1s;}\n';
   html_code += 'nobr { font-size: 14px; color: #f6f6f6; font-style: italic; text-shadow: none; }\n';
+  html_code += 'div.photo img { border: medium none; max-width: 100%; padding: 0;}\n';
   html_code += '</style>\n';
-  html_code += '</head>\n';
-  html_code += '<body>\n';
 
   html_code += '<div id="container">\n';
   html_code += '<div id="items">\n';
@@ -86,41 +82,28 @@ var MN_infocus = (function () {
       if(current_image.width < 1280 && current_image.width > 1024){
           k++;
           html_code += '<div class="item">\n';
-          html_code += '<h2>Photo ' + k + ' of ' + total_images + '</h2>\n';
+          html_code += '<div class="gallery_date">Photo ' + k + ' of ' + total_images + '</div>\n';
           html_code += '<div class="photo">';
           html_code += '<img src="' + current_image.src + '" width="' + current_image.width + '" height="' + current_image.height + '" alt="" />';
           html_code += '</div>\n';
-          html_code += '<p class="caption">' + $(current_caption).html() + '</p>\n</div>' + '\n\n';
+          html_code += '<p class="gallery_caption">' + $(current_caption).html() + '</p>\n</div>' + '\n\n';
       }
   }
 
   html_code += '</div>\n';
   html_code += '</div>\n';
 
-  html_code += '</body>\n';
-  html_code += '</html>\n';
-
 // Write the new html gallery code
-  document.open;
-  document.write(html_code);
-  document.close;
+  $('body').html(html_code);
 
-// Load the jQuery library and the jQuery Cycle plugin
+// Load the jQuery Cycle plugin
   var script = document.createElement("script");
-    script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
+    script.setAttribute("src", "http://www.marlenynunez.com/files/infocus/jquery.cycle.all.js");
     script.addEventListener('load', function() {
       var script = document.createElement("script");
       document.body.appendChild(script);
+      $('#items').cycle({ fx: 'scrollUp', timeout: 12000, delay:  -2000, fit: true, height: 700, width: 1200 });
     }, false);
     document.body.appendChild(script);
-
-  var script2 = document.createElement("script");
-    script2.setAttribute("src", "http://www.marlenynunez.com/files/infocus/jquery.cycle.all.js");
-    script2.addEventListener('load', function() {
-      var script2 = document.createElement("script");
-      document.body.appendChild(script2);
-      $('#items').cycle({ fx: 'scrollUp', timeout: 6000, delay:  -2000, fit: true, height: 700, width: 1200 });
-    }, false);
-    document.body.appendChild(script2);
 
 }());
